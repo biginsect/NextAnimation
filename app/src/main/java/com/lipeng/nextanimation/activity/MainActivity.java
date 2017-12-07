2bb8d2de132c290dc0768d9a05fdc940ff52fd3c
lipeng <lipeng-ds3@gomeplus.com>
2017-12-07 10:49:56 +0800

    init

---
+++ app/src/main/java/com/lipeng/nextanimation/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lipeng.nextanimation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lipeng.nextanimation.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
